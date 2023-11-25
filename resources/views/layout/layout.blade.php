aefe1928c2a65deea48fba871e638947ce831e6a
bwire40 <emmanuelbwire80@gmail.com>
2023-11-25 17:24:45 +0300

    Finished on create post form

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -13,6 +13,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"
         integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
 
+    @stack('styles')
     <!-- tailwind css -->
     @vite('resources/css/app.css', 'resources/js/app.js')
 
@@ -58,6 +59,8 @@
             }
         }
     </script>
+
+    @stack('scripts')
 
 </body>
 
--- resources/views/partials/create_post_form.blade.php
+++ resources/views/partials/create_post_form.blade.php
@@ -1,25 +1,19 @@
 @extends('layout.layout')
+@push('styles')
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/easymde/dist/easymde.min.css">
+@endpush
 @section('content')
     <div class="container mx-auto p-4">
         <form>
             <div class="space-y-12">
-                <h1 class="text-5xl font-bold text-slate-600">Create Post</h1>
+                <h1 class="text-5xl font-semibold text-slate-600">Create Post</h1>
                 <div class="border-b border-gray-900/10 pb-12">
 
                     <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                         <div class="sm:col-span-3">
-                            <label for="first-name" class="block   leading-6 text-gray-900 font-bold text-xl">Title</label>
+                            <label for="title" class="block   leading-6 text-gray-900 font-bold text-xl">Title</label>
                             <div class="mt-2">
-                                <input type="text" name="first-name" id="first-name" autocomplete="given-name"
-                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 font-bold text-xl shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm: sm:leading-6">
-                            </div>
-                        </div>
-
-                        <div class="sm:col-span-3">
-                            <label for="last-name"
-                                class="block   leading-6 text-gray-900 font-bold text-xl">Sub-title</label>
-                            <div class="mt-2">
-                                <input type="text" name="last-name" id="last-name" autocomplete="family-name"
+                                <input type="text" name="title" id="title" autocomplete="given-name"
                                     class="block w-full rounded-md border-0 py-1.5 text-gray-900 font-bold text-xl shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm: sm:leading-6">
                             </div>
                         </div>
@@ -43,13 +37,17 @@
                     </div>
 
                     <div class="col-span-full">
-                        <label for="about" class="block   leading-6 text-gray-900 font-bold text-xl">Content</label>
-                        <div class="mt-2">
-                            <textarea id="about" name="about" rows="3"
-                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 font-bold text-xl shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm: sm:leading-6"></textarea>
+                        <label for="content"
+                            class="block leading-6 text-gray-900 font-bold text-xl pt-5 pb-3">Content</label>
+                        <div class="mb-6">
+                            <textarea id="markdown-editor" class="block w-full mt-1 rounded-md" name="content" rows="3"></textarea>
+                            @error('description')
+                                <div class="text-sm text-red-600">{{ $message }}</div>
+                            @enderror
                         </div>
-                        <p class="mt-3  leading-6 text-gray-600">Write a few sentences about your post.</p>
+                        <p class="mt-2 leading-6 text-gray-600">Write a few sentences about your post.</p>
                     </div>
+
                 </div>
 
                 <div class="col-span-full">
@@ -80,7 +78,7 @@
                 <button type="reset" class=" font-semibold leading-6 text-gray-900 text-sm">Reset <button>
                         <button type="submit"
                             class="rounded-md bg-indigo-600 px-3 py-2  font-semibold text-white shadow-sm
-                            hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+                            hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Post</button>
             </div>
     </div>
 
@@ -91,4 +89,16 @@
 
     </form>
     </div>
+
+    @push('scripts')
+        <script src="https://cdn.jsdelivr.net/npm/easymde/dist/easymde.min.js"></script>
+        <script>
+            const easyMDE = new EasyMDE({
+                showIcons: ['strikethrough', 'code', 'table', 'redo', 'heading', 'undo', 'heading-bigger',
+                    'heading-smaller', 'heading-1', 'heading-2', 'heading-3', 'clean-block', 'horizontal-rule'
+                ],
+                element: document.getElementById('markdown-editor')
+            });
+        </script>
+    @endpush
 @endsection
